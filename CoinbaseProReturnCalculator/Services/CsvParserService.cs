ae92ade504628fbf4c625a4dda2a799a7c277053
rjeavons <rickyjeavons@tfl.gov.uk>
2021-02-16 20:16:04 +0000

    First commit

---
+++ CoinbaseProReturnCalculator/Services/CsvParserService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+
+namespace CoinbaseProReturnCalculator
+{
+    public class CsvParserService
+    {
+        public List<CoinbaseCsvModel> ReadCsvFile(string fileName)
+        {
+            var reader = new StreamReader($"Data/{fileName}");
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            {
+                var records = new List<CoinbaseCsvModel>();
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var record = new CoinbaseCsvModel
+                    {
+                        Portfolio = csv.GetField("portfolio"),
+                        TradeId = csv.GetField<int>("trade id"),
+                        Product = csv.GetField("product"),
+                        Side = csv.GetField("side"),
+                        CreatedAt = csv.GetField("created at"),
+                        Size = csv.GetField<decimal>("size"),
+                        SizeUnit = csv.GetField("size unit"),
+                        Price = csv.GetField<decimal>("price"),
+                        Fee = csv.GetField<decimal>("fee"),
+                        Total = csv.GetField<decimal>("total"),
+                        Pftu = csv.GetField("price/fee/total unit")
+                    };
+                    records.Add(record);
+                }
+                return records;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-CoinbaseProReturnCalc
+CoinbaseProReturnCalculator
+
+Visit https://pro.coinbase.com/orders/filled
+
+Scroll down to the bottom of the table and choose: "Displaying past 90 days. To see full history, download statements."
+
+In the pop-up, choose:
+
+Product*
+All Products
+
+Start Date*
+Earliest transaction date
+
+End Date*
+Today's date
+
+Format*
+CSV
+
+Email*
+your email
+
+The statement will be sent to your email address. You can download the CSV file from here.
+
+Place the CSV in the Data folder
